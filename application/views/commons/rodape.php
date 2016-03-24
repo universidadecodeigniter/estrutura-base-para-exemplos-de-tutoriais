a9e09975774830b1876c6cfe0aef9f379a836ef3
JLamim Projetos Web & Mobile <dev@jlamim.com.br>
2016-03-24 11:46:13 -0300

    Arquivos da estrutura base

---
+++ application/views/commons/rodape.php
@@ -0,0 +1,4 @@
+<!-- IE10 viewport hack para Surface/desktop Windows 8 -->
+<script src="<?=base_url('assets/js/plugins/ie10-viewport-bug-workaround.js')?>"></script>
+</body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,25 @@
+<?php $this->load->view('commons/cabecalho'); ?>
+
+<div class="container">
+	<div class="page-header">
+		<h1>Estrutura base para exemplos de tutorias</h1>
+	</div>
+	<p class="lead">Essa é a estrutura base para exemplos de tutoriais do portal <strong>Universidade CodeIgniter</strong>.</p>
+	<p>A estrutura é composta por:</p>
+		<ul>
+			<li>
+				<a href="http://codeigniter.com" target="_blank" title="Site Oficial do CodeIgniter">CodeIgniter</a>, na versão 3.0.6
+			</li>
+			<li>
+				<a href="http://getbootstrap.com" target="_blank" title="Site Oficial do Bootstrap">Bootstrap</a>, na versão 3.3.6
+			</li>
+			<li>
+				<a href="http://jquery.com" target="_blank" title="Site Oficial do jQuery">jQuery</a>, na versão 1.12.2
+			</li>
+		</ul>
+		<p>
+			Se no seu tutorial precisar utilizar outros plugins ou libraries, basta adicioná-los no repositório <code>assets/js/plugins</code> e/ou <code>assets/css/plugins</code>, conforme o tipo do arquivo.
+		</p>
+</div>
+
+<?php $this->load->view('commons/rodape'); ?>
